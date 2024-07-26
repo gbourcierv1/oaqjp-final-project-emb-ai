1be99df764128be5ac281541d4d7de4a3e7ba00f
gbourcierv1 <geobourcier@gmail.com>
2024-07-26 12:48:29 -0700

    added files

--- emotion_detection.py
+++ emotion_detection.py
@@ -15,6 +15,19 @@ def emotion_detector(text_to_analyze):
     }
     response = requests.post(WATSON_URL, headers=HEADERS, json=data)
     if response.status_code == 200:
-        return response.json()
+        emotions = response.json().get('emotion_predictions', [{}])[0].get('emotion', {})
+        if emotions:
+            emotion_scores = {
+                'anger': emotions.get('anger', 0),
+                'disgust': emotions.get('disgust', 0),
+                'fear': emotions.get('fear', 0),
+                'joy': emotions.get('joy', 0),
+                'sadness': emotions.get('sadness', 0),
+            }
+            dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+            emotion_scores['dominant_emotion'] = dominant_emotion
+            return emotion_scores
+        else:
+            return {"error": "No emotion data found"}
     else:
         return {"error": "Unable to fetch emotion data"}
